ae5937fa3c1348126dac0de86eb3e4f742703a40
sauronmaster24 <50328255+sauronmaster24@users.noreply.github.com>
2022-02-24 00:08:12 +0100

    Update settings.py

--- uploads/settings.py
+++ uploads/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
